d22ff642a1f28561d819f8671de63d3d2330c000
Tom Adriaenssen <spam@inferis.org>
2011-04-24 11:14:36 +0200

    Filling Twunch detail with data.

--- Twunch/UI/TwunchDetailViewModel.cs
+++ Twunch/UI/TwunchDetailViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,9 +10,34 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Caliburn.Micro;
+using Inferis.TwunchApp.API;
 
 namespace Inferis.TwunchApp.UI {
-    public class TwunchDetailViewModel {
+    public class TwunchDetailViewModel : Screen {
+        private Twunch item;
+
         public string Id { get; set; }
+        public string Title { get { return item == null ? "" : item.Title; } }
+        public string Date { get { return item == null ? "" : item.Date.ToLocalTime().ToString("dd/MM/yyyy HH:mm"); } }
+        public List<string> Participants { get { return item == null ? null : item.Participants; } }
+        
+        protected Twunch Item
+        {
+            get { return item; }
+            set
+            {
+                item = value;
+                NotifyOfPropertyChange(() => Title);
+                NotifyOfPropertyChange(() => Participants);
+                NotifyOfPropertyChange(() => Date);
+            }
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            new Twunch.Fetcher(twunches => Item = twunches.FirstOrDefault(x => x.Id == Id), true);
+        }
     }
 }
